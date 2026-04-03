fe016974c1d7a02e6ed36a7ab4b40e7923910925
ridonandu-cmyk <ridonandu@gmail.com>
2026-04-03 13:05:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,39 @@ function createBot() {
 
   bot.on('spawn', () => {
     console.log('Bot joined');
-  });
 
-  setInterval(() => {
-    bot.setControlState('jump', true);
-    setTimeout(() => bot.setControlState('jump', false), 500);
-  }, 30000);
+    moveBot(bot);
+  });
 
   bot.on('end', () => {
     setTimeout(createBot, 5000);
   });
+}
+
+// 🔥 Movement function
+function moveBot(bot) {
+  const actions = ['forward', 'back', 'left', 'right'];
+
+  setInterval(() => {
+    const action = actions[Math.floor(Math.random() * actions.length)];
+
+    bot.setControlState(action, true);
+
+    // stop after 2 sec
+    setTimeout(() => {
+      bot.setControlState(action, false);
+    }, 2000);
+
+    // random jump
+    bot.setControlState('jump', true);
+    setTimeout(() => bot.setControlState('jump', false), 500);
+
+    // random look (rotation)
+    const yaw = Math.random() * Math.PI * 2;
+    const pitch = (Math.random() - 0.5) * Math.PI / 2;
+    bot.look(yaw, pitch);
+
+  }, 5000);
 }
 
 createBot();
